c7c5767b173565088d392a438f4d62579692ac9d
star sky <1176789241@qq.com>
2020-05-12 16:05:06 +0800

    添加PageHeder组件

---
+++ src/views/PersonCenter/components/PageHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page-header-container">
+    <div class="page-header-text">{{text}}</div>
+    <hr class="page-header-underline" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageHeader",
+  props: {
+    text: {
+      type: String,
+      required: false,
+      default: "Hello World"
+    }
+  }
+};
+</script>
+
+<style scoped>
+.page-header-container {
+  margin: 40px auto;
+  width: 95%;
+}
+.page-header-text {
+  margin-left: 40px;
+  font-size: 22px;
+  color: #303133;
+}
+.page-header-underline {
+  border: 0;
+  border-bottom: 2px solid rgba(248, 212, 212, 0.959);
+}
+</style>
