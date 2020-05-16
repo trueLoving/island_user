462fd7cde43474e9848ad72a1ec7276aa6e9efcb
star sky <1176789241@qq.com>
2020-05-16 22:09:35 +0800

    :construction: 富文本构建初步完成

---
+++ src/components/RichEditor/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="rich-editor-container">
+    <vue-editor v-model="content"></vue-editor>
+    <div class="toolbar">
+      <b-button variant="danger" class="reset-button" @click="reset">重置</b-button>
+      <b-button variant="primary" class="submit-button" @click="submit">提交</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { VueEditor } from "vue2-editor";
+
+export default {
+    name:"RichEditor",
+    components:{
+        VueEditor
+    },
+    props:{
+        c:{
+            type:String,
+            required:false,
+            default:""
+        }
+    },
+    data(){
+        return{
+            content:""
+        }
+    },
+    methods:{
+        submit(){
+            // todo submit
+        },
+        reset(){
+            this.content = "";
+        }
+    },
+    mounted(){
+        if(!this.c)this.content = this.c;
+    }
+}
+</script>
+
+<style scoped>
+.rich-editor-container {
+  padding: 6px;
+}
+.toolbar {
+  margin: 20px;
+  text-align: center;
+}
+.reset-button,
+.submit-button {
+  margin: 20px;
+}
+</style>
