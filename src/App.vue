4dfe6babb3cd5696e882d665d5935f8223e3e7a9
star sky <1176789241@qq.com>
2020-05-17 10:31:42 +0800

    :lipstick: 添加首页初次加载动画

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="app">
-    <router-view v-if="isRouterAlive"/>
+    <router-view v-if="isRouterAlive" />
+    <LoadWrapper v-if="isLoading" />
   </div>
 </template>
 
 <script>
+
+import LoadWrapper from "@/components/LoadWrapper";
+
 export default {
+  components:{
+    LoadWrapper
+  },
   name: 'App',
   provide(){
     return{
@@ -14,7 +21,8 @@ export default {
   },
   data(){
     return{
-      isRouterAlive:true
+      isRouterAlive:true,
+      isLoading:true
     }
   },
   methods:{
@@ -24,6 +32,9 @@ export default {
         this.isRouterAlive = true;
       })
     }
+  },
+  mounted(){
+    this.isLoading = false;
   }
 }
 </script>
